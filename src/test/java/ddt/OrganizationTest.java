cc36091bbd6da3e1935a434154a55c422e55ee69
ChaitraQsp <chaitraqspvirtual@gmail.com>
2022-09-12 12:46:57 +0530

    Commit and push by user 1

---
+++ src/test/java/ddt/OrganizationTest.java
@@ -0,0 +1,25 @@
+package ddt;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("Organization created");
+	}
+
+	@Test
+	public void modifyOrgTest()
+	{
+		System.out.println("Oraganization Modified");
+		
+	}
+	
+	@Test
+	public void deleteOrgTest()
+	{
+		System.out.println("Organization deleted");
+	}
+}
